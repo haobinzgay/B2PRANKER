c049a5abf798d461a4ad8746a7a414140bc97c9b
haobinzgay <jamescdaga@Gmail.com>
2025-06-12 20:06:41 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -390,7 +390,7 @@ function removeRankedTrainee(trainee) {
   return false;
 }
 
-const currentURL = "https://9thdesire.github.io/BoysPlanetII/";
+const currentURL = "https://9thpick.github.io/B2PRANKER/;
 // Serializes the ranking into a string and appends that to the current URL
 function generateShareLink() {
   let shareCode = ranking.map(function (trainee) {
